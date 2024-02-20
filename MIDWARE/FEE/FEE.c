cf64323ca63f30dea4900410e3a680b31c15ac26
Khaled Magdy <admin@deepbluembedded.com>
2024-02-20 10:45:31 +0200

    Added MIDWR/FEE basic FLASH wrappers for FEE (Flash EEPROM Emulation)

---
+++ MIDWARE/FEE/FEE.c
@@ -0,0 +1,72 @@
+/*
+ * File: FEE.c
+ * Driver Name: [[ FEE (Flash EEPROM Emulation) ]]
+ * SW Layer:   MIDWR
+ * Author:     Khaled Magdy
+ * -------------------------------------------
+ * For More Information, Tutorials, etc.
+ * Visit Website: www.DeepBlueMbedded.com
+ */
+#include "FEE.h"
+#include "string.h"
+
+HAL_StatusTypeDef FEE_Write(uint32_t address, uint16_t *data, uint32_t dataSize)
+{
+    HAL_StatusTypeDef status = HAL_OK;
+    uint32_t pageAddress = address & ~(FLASH_PAGE_SIZE - 1); // Get the start address of the page
+    uint16_t buffer[FEE_BUFFER_LEN]; // Create a buffer to hold the entire page
+    HAL_FLASH_Unlock();
+    // Read the entire page into the buffer
+    for (uint32_t i = 0; i < FEE_BUFFER_LEN; i++) {
+        buffer[i] = *(__IO uint16_t*)(pageAddress + i * 2);
+    }
+    // Modify the values at the desired address
+    uint32_t offset = address - pageAddress;
+    for (uint32_t i = 0; i < dataSize; i++) {
+        buffer[offset / 2 + i] = data[i];
+    }
+    // Erase the page
+    FLASH_EraseInitTypeDef eraseInitStruct;
+    eraseInitStruct.TypeErase = FLASH_TYPEERASE_PAGES;
+    eraseInitStruct.PageAddress = pageAddress;
+    eraseInitStruct.NbPages = 1;
+    uint32_t pageError;
+    status = HAL_FLASHEx_Erase(&eraseInitStruct, &pageError);
+    if (status != HAL_OK) {
+        HAL_FLASH_Lock();
+        return status;
+    }
+    // Write the modified buffer back to the page
+    for (uint32_t i = 0; i < FEE_BUFFER_LEN; i++) {
+        status = HAL_FLASH_Program(FLASH_TYPEPROGRAM_HALFWORD, pageAddress + i * 2, buffer[i]);
+        if (status != HAL_OK) {
+            break;
+        }
+    }
+    HAL_FLASH_Lock();
+    return status;
+}
+
+void FEE_Read(uint32_t address, uint16_t *data, uint32_t dataSize)
+{
+    for (uint32_t i = 0; i < dataSize; i++) {
+        data[i] = *(__IO uint16_t*)address;
+        address += 2;
+    }
+}
+
+// Generic APIs For Any Data Type
+void FEE_WriteData(uint32_t address, void *data, size_t dataSize)
+{
+    uint16_t bytes[dataSize / 2];
+    memcpy(bytes, data, dataSize);
+    FEE_Write(address, bytes, dataSize);
+}
+
+void FEE_ReadData(uint32_t address, void *data, size_t dataSize)
+{
+    uint16_t bytes[dataSize / 2];
+    FEE_Read(address, bytes, dataSize);
+    memcpy(data, bytes, dataSize);
+}
+
---
+++ MIDWARE/FEE/FEE.h
@@ -0,0 +1,32 @@
+/*
+ * File: FEE.h
+ * Driver Name: [[ FEE (Flash EEPROM Emulation) ]]
+ * SW Layer:   MIDWR
+ * Author:     Khaled Magdy
+ * -------------------------------------------
+ * For More Information, Tutorials, etc.
+ * Visit Website: www.DeepBlueMbedded.com
+ */
+#ifndef FEE_H_
+#define FEE_H_
+/*
+ * Disclaimer: This library is a quick & dirty implementation for FLASH wrapper functions
+ * That may be helpful for basic applications, not for use in anything serious obviously.
+ */
+#include "stdint.h"
+#include "main.h"
+
+#define FEE_START_ADDRESS	((uint32_t)0x0800F000)  // FEE Start Address (in Flash Memory)
+#define FEE_PAGE_SIZE		((uint32_t)0x400)       // FEE Page Size = 1kB (Default Page Size For STM32F103)
+#define FEE_BUFFER_LEN		(FEE_PAGE_SIZE / 2U)
+
+//-----[ Prototypes For All User External Functions ]-----
+
+HAL_StatusTypeDef FEE_Write(uint32_t address, uint16_t *data, uint32_t dataSize);
+void FEE_Read(uint32_t address, uint16_t *data, uint32_t dataSize);
+
+// Generic APIs For Any Data Type
+void FEE_WriteData(uint32_t address, void *data, size_t dataSize);
+void FEE_ReadData(uint32_t address, void *data, size_t dataSize);
+
+#endif /* FEE_H_ */
